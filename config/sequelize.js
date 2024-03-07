5c54178171a38516928516b7f21a5ee717794ded
gustavoisaacc <gustavochaile7@gmail.com>
2024-03-07 15:05:08 -0300

    first commit

---
+++ config/sequelize.js
@@ -0,0 +1,11 @@
+const configDB = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+};
+
+module.exports = { configDB };
