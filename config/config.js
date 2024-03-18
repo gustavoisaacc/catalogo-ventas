76553fbe53c3760e0e43753731315c621ef31229
gustavoisaacc <gustavochaile7@gmail.com>
2024-03-18 08:21:07 -0300

    change in variable enbairomen

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ config();
 const configDB = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  dbName: process.env.DB_NAME,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
+  dbName: process.env.PG_DATABASE,
+  dbUser: process.env.PG_USER,
+  dbPassword: process.env.PG_PASSWORD,
+  dbHost: process.env.PG_HOST,
+  dbPort: process.env.PG_PORT,
 };
 
 module.exports = { configDB };
