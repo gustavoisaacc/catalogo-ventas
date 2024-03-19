eb003a859d169a14084da30a19475d222bfc40db
gustavoisaacc <gustavochaile7@gmail.com>
2024-03-19 06:32:13 -0300

    change in file config url

--- db/config.js
+++ db/config.js
@@ -1,16 +1,12 @@
 const { configDB } = require("../config/config");
 
-const USER = encodeURIComponent(configDB.dbUser);
-const PASSWORD = encodeURIComponent(configDB.dbPassword);
-const URL = `postgres://${USER}:${PASSWORD}@${configDB.dbHost}:${configDB.dbPort}/${configDB.dbName}`;
-
 module.exports = {
   development: {
-    url: URL,
+    url: configDB.dbUrl,
     dialect: "postgres",
   },
   production: {
-    url: URL,
+    url: configDB.dbUrl,
     dialect: "postgres",
   },
 };
